1c9996e85aef2b8b00f482248c85452104d85fbb
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2006-09-12 02:38:49 +0000

    select.h
    git-svn-id: file:///source/coreland/io_poll/trunk@1063 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- insthier.c
+++ insthier.c
@@ -9,6 +9,7 @@ struct install_item insthier[] = {
   {INST_COPY, "_aio-mech.h", 0, ctxt_incdir, ctxt_owner, ctxt_group, 0644},
   {INST_COPY, "_select.h", 0, ctxt_incdir, ctxt_owner, ctxt_group, 0644},
   {INST_COPY, "aio-mech.h", 0, ctxt_incdir, ctxt_owner, ctxt_group, 0644},
+  {INST_COPY, "select.h", 0, ctxt_incdir, ctxt_owner, ctxt_group, 0644},
   {INST_COPY, "io_poll_fdh.h", 0, ctxt_incdir, ctxt_owner, ctxt_group, 0644},
   {INST_COPY, "io_poll.a", 0, ctxt_slibdir, ctxt_owner, ctxt_group, 0644},
   {INST_SYMLINK, "io_poll.a", "libio_poll.a", ctxt_slibdir, ctxt_owner, ctxt_group, 0644},
