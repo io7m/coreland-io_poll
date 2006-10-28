fb17cc56ce1c42bbf46762914790a9b39524e3a5
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2006-10-28 23:42:37 +0000

    no symlink
    git-svn-id: file:///source/coreland/io_poll/trunk@1199 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- insthier.c
+++ insthier.c
@@ -21,8 +21,7 @@ struct install_item insthier[] = {
   {INST_COPY, "io_poll_fdh.h", 0, ctxt_incdir, 0, 0, 0444},
   {INST_COPY, "select.h", 0, ctxt_incdir, 0, 0, 0444},
 
-  {INST_COPY, "io_poll.a", 0, ctxt_slibdir, 0, 0, 0644},
-  {INST_SYMLINK, "io_poll.a", "libio_poll.a", ctxt_slibdir, 0, 0, 0644},
+  {INST_COPY, "io_poll.a", "libio_poll.a", ctxt_slibdir, 0, 0, 0644},
   {INST_COPY, "io_poll-conf", 0, ctxt_bindir, 0, 0, 0755},
 };
 unsigned long insthier_len = sizeof(insthier) / sizeof(struct install_item);
